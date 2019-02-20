4a1bc2028f497237b3680f65e9ffba66065590a8
Artem Kivachuk <mamokosf@gmail.com>
2019-02-20 22:31:14 +0300

    first solving

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -6,8 +6,25 @@ namespace PadawansTask11
     {
         public static int? FindIndex(double[] array, double accuracy)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array.Length == 0)
+                throw new ArgumentException();
+
+            for(int i=1; i<array.Length - 1; i++)
+            {
+                double num1 = 0, num2 = 0;
+                for(int j=0; j<array.Length; j++)
+                {
+                    if (j < i)
+                        num1 += array[j];
+                    if (j > i)
+                        num2 += array[j];
+                }
+                if (Math.Abs(num2 - num1) <= accuracy)
+                    return i;
+            }
+            return null;
         }
     }
 }
